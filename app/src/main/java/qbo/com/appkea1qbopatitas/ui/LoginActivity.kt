7f5d48284e94621297f0b9d5391475f5dce51fcc
LuisAngel <luisangel.salvat@gmail.com>
2020-11-22 15:18:06 -0500

    Actualización del proyecto Appatitas

--- app/src/main/java/qbo/com/appkea1qbopatitas/ui/LoginActivity.kt
+++ app/src/main/java/qbo/com/appkea1qbopatitas/ui/LoginActivity.kt
@@ -1,12 +1,94 @@
 package qbo.com.appkea1qbopatitas.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_login.*
+import org.json.JSONObject
 import qbo.com.appkea1qbopatitas.R
 
 class LoginActivity : AppCompatActivity() {
+    //Definimos el objeto RequestQueue
+    private lateinit var colaPeticiones: RequestQueue
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        //Haciendo la instancia al objeto RequestQueue
+        colaPeticiones = Volley.newRequestQueue(this)
+
+        btnregistrar.setOnClickListener {
+            startActivity(Intent(this,
+                    RegistroActivity::class.java))
+        }
+
+        btnlogin.setOnClickListener {
+            btnlogin.isEnabled = false
+            pblogin.visibility = View.VISIBLE
+            if(validarUsuarioPassword()){
+                //Realizamos la petición a la API Rest de Login
+                autenticarUsuarioApiRest(it,
+                        etusuario.text.toString(),
+                        etpassword.text.toString())
+            }else{
+                btnlogin.isEnabled = true
+                pblogin.visibility = View.INVISIBLE
+                mostrarMensaje(it, getString(R.string.msgerrorusuriopass))
+            }
+        }
+    }
+
+    private fun autenticarUsuarioApiRest(vista: View, usuario: String, password: String){
+        val urlapirestlogin = "http://www.kreapps.biz/patitas/login.php"
+        val parametros = JSONObject()
+        parametros.put("usuario", usuario)
+        parametros.put("password", password)
+        val request = JsonObjectRequest(
+                Request.Method.POST,
+                urlapirestlogin,
+                parametros,
+                { response ->
+                    if(response.getBoolean("rpta")){
+                        startActivity(Intent(this,
+                                MainActivity::class.java))
+                        finish()
+                    }else{
+                        mostrarMensaje(vista, response.getString("mensaje"))
+                    }
+                    btnlogin.isEnabled = true
+                    pblogin.visibility = View.INVISIBLE
+                },{ error ->
+            Log.i("ErorLogin", error.message.toString())
+            mostrarMensaje(vista, getString(R.string.msgerrorapilogin))
+            btnlogin.isEnabled = true
+            pblogin.visibility = View.INVISIBLE
+        })
+
+        colaPeticiones.add(request)
+    }
+
+    fun validarUsuarioPassword(): Boolean{
+        var respuesta = true
+        if(etusuario.text.toString().trim().isEmpty()){
+            etusuario.isFocusableInTouchMode = true
+            etusuario.requestFocus()
+            respuesta = false
+        }else if(etpassword.text.toString().trim().isEmpty()){
+            etpassword.isFocusableInTouchMode = true
+            etpassword.requestFocus()
+            respuesta = false
+        }
+        return respuesta
+    }
+
+    fun mostrarMensaje(vista: View, mensaje: String){
+        Snackbar.make(vista, mensaje, Snackbar.LENGTH_LONG).show()
     }
 }
--- app/src/main/java/qbo/com/appkea1qbopatitas/ui/RegistroActivity.kt
+++ app/src/main/java/qbo/com/appkea1qbopatitas/ui/RegistroActivity.kt
@@ -2,11 +2,129 @@ package qbo.com.appkea1qbopatitas.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_registro.*
+import org.json.JSONObject
 import qbo.com.appkea1qbopatitas.R
 
 class RegistroActivity : AppCompatActivity() {
+
+    private lateinit var colaPeticiones : RequestQueue
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
+        colaPeticiones = Volley.newRequestQueue(this)
+        btnregistrarusuario.setOnClickListener {
+            btnregistrarusuario.isEnabled = false
+            pbregistro.visibility = View.VISIBLE
+            if(validarFormulario(it)){
+                registrarUsuarioApiRest(it)
+            }else{
+                btnregistrarusuario.isEnabled = true
+                pbregistro.visibility = View.GONE
+            }
+        }
+    }
+
+    private fun registrarUsuarioApiRest(vista: View){
+        val urlapirestregistro = "http://www.kreapps.biz/patitas/persona.php"
+        val parametro = JSONObject()
+        parametro.put("nombres", etnombrereg.text.toString())
+        parametro.put("apellidos", etapellidoreg.text.toString())
+        parametro.put("email", etemailreg.text.toString())
+        parametro.put("celular", etcelularreg.text.toString())
+        parametro.put("usuario", etusuarioreg.text.toString())
+        parametro.put("password", etpasswordreg.text.toString())
+        val request = JsonObjectRequest(
+            Request.Method.PUT,
+            urlapirestregistro,
+            parametro,
+            { response ->
+                if(response.getBoolean("rpta")){
+                    setearControles()
+                }
+                mostrarMensaje(vista, response.getString("mensaje"))
+                btnregistrarusuario.isEnabled = true
+                pbregistro.visibility = View.GONE
+            },{ error->
+                Log.e("ErrorRegUsu", error.message.toString())
+                btnregistrarusuario.isEnabled = true
+                pbregistro.visibility = View.GONE
+            }
+        )
+        colaPeticiones.add(request)
+    }
+
+    private fun setearControles(){
+        etnombrereg.setText("")
+        etapellidoreg.setText("")
+        etemailreg.setText("")
+        etcelularreg.setText("")
+        etusuarioreg.setText("")
+        etpasswordreg.setText("")
+        etpasswordreg2.setText("")
+    }
+
+    private fun validarFormulario(vista: View): Boolean{
+        var respuesta = true
+        when{
+            etnombrereg.text.toString().trim().isEmpty() -> {
+                etnombrereg.isFocusableInTouchMode = true
+                etnombrereg.requestFocus()
+                mostrarMensaje(vista, getString(R.string.msgerrordatosreg))
+                respuesta = false
+            }
+            etapellidoreg.text.toString().trim().isEmpty() -> {
+                etapellidoreg.isFocusableInTouchMode = true
+                etapellidoreg.requestFocus()
+                mostrarMensaje(vista, getString(R.string.msgerrordatosreg))
+                respuesta = false
+            }
+            etemailreg.text.toString().trim().isEmpty() -> {
+                etemailreg.isFocusableInTouchMode = true
+                etemailreg.requestFocus()
+                mostrarMensaje(vista, getString(R.string.msgerrordatosreg))
+                respuesta = false
+            }
+            etcelularreg.text.toString().trim().isEmpty() -> {
+                etcelularreg.isFocusableInTouchMode = true
+                etcelularreg.requestFocus()
+                mostrarMensaje(vista, getString(R.string.msgerrordatosreg))
+                respuesta = false
+            }
+            etusuarioreg.text.toString().trim().isEmpty() -> {
+                etusuarioreg.isFocusableInTouchMode = true
+                etusuarioreg.requestFocus()
+                mostrarMensaje(vista, getString(R.string.msgerrordatosreg))
+                respuesta = false
+            }
+            etpasswordreg.text.toString().trim().isEmpty() -> {
+                etpasswordreg.isFocusableInTouchMode = true
+                etpasswordreg.requestFocus()
+                mostrarMensaje(vista, getString(R.string.msgerrordatosreg))
+                respuesta = false
+            }
+            etpasswordreg.text.toString().trim().isNotEmpty() -> {
+                if(etpasswordreg.text.toString() != etpasswordreg2.text.toString()){
+                    etpasswordreg.isFocusableInTouchMode = true
+                    etpasswordreg.requestFocus()
+                    mostrarMensaje(vista, getString(R.string.msgerrorpasswordreg))
+                    respuesta = false
+                }
+
+            }
+        }
+        return respuesta
+    }
+
+    private fun mostrarMensaje(vista: View, mensaje: String){
+        Snackbar.make(vista, mensaje, Snackbar.LENGTH_LONG).show()
     }
 }
